80700854c3dfdca28145d1e202e7593017c6a446
bobi.rival <bahmadrival@gmail.com>
2024-04-30 11:12:33 +0700

    add middleware

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"belajar-golang-restfulapi/controller"
 	"belajar-golang-restfulapi/exception"
 	"belajar-golang-restfulapi/helper"
+	"belajar-golang-restfulapi/middleware"
 	"belajar-golang-restfulapi/repository"
 	"belajar-golang-restfulapi/service"
 	"net/http"
@@ -33,7 +34,7 @@ func main() {
 
 	server := http.Server{
 		Addr:    "localhost:3000",
-		Handler: router,
+		Handler: middleware.NewAuthMiddleware(router),
 	}
 
 	err := server.ListenAndServe()
